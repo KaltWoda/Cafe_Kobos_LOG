f8b8d1ab2d1d8e96a20cf40861cc4cbd614865f9
USER <kaltwodamc@gmail.com>
2025-08-12 12:27:26 -0600

    tabla usuarios

--- js/login.js
+++ js/login.js
@@ -3,7 +3,7 @@ async function login() {
   const password = document.getElementById("password").value.trim();
 
   const { data, error } = await supabaseClient
-    .from("Usuarios")
+    .from("usuarios")
     .select("*")
     .eq("username", username)
     .eq("password", password)
